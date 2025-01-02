2262434431985698567cb0bfe76e9600ef42c797
Rikard Engström <rikardengstrom89@gmail.com>
2025-01-02 18:29:40 +0100

    testing

--- pages/index/header.js
+++ pages/index/header.js
@@ -75,12 +75,13 @@ function createNavigation(headerData) {
     const menuItem = document.createElement('li');
     const menuLink = document.createElement('a');
     //Using a switch that adds the href attribute depending on the link text. This can be updated when we get more webpages online.
+    const basePath = '/Kino_Group_Project';
     switch (link.text) {
       case 'Om oss':
-        menuLink.href = '/pages/about/about.html';
+        menuLink.href = `${basePath}/pages/about/about.html`;
         break;
       case 'Barnkalas':
-        menuLink.href = '/pages/kids/kids.html';
+        menuLink.href = `${basePath}/pages/kids/kids.html`;
         break;
       default:
         menuLink.href = '#';
