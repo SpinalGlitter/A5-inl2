d54c4bdb846940472bbffb4449cee269245a1531
Rikard Engström <rikardengstrom89@gmail.com>
2025-01-03 19:36:55 +0100

    fixed the last stuff i think.

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import { initHeader } from './src/javascript/header.js';
 import { loadkids } from './src/javascript/kids.js';
 import { loadMovieContent } from './src/javascript/movies.js';
 import { buildDoc } from './src/javascript/infoModal.js';
-
+import './src/javascript/footer.js';
+import { updateDomWithAboutJson } from './src/javascript/about.js';
 initHeader();
 
 const checkKids = document.querySelector('.article-kids');
